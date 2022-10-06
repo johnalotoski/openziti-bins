3019acfba44d6048238fbc40264ed3e9f88c3e50
John Lotoski <john.lotoski@iohk.io>
2022-10-05 19:13:53 -0500

    imp: add main ziti bins

--- flake.nix
+++ flake.nix
@@ -30,8 +30,32 @@
       '';
 
       meta = with lib; {
-        homepage = "https://openziti.github.io";
+        homepage = "https://github.com/openziti/ziti-tunnel-sdk-c";
         description = "Ziti: programmable network overlay and associated edge components for application-embedded, zero-trust networking";
+        platforms = platforms.linux;
+      };
+    };
+
+    packages.x86_64-linux.ziti = stdenv.mkDerivation rec {
+      name = "ziti-${version}";
+      version = "0.26.8";
+
+      src = pkgs.fetchurl {
+        url = "https://github.com/openziti/ziti/releases/download/v${version}/ziti-linux-amd64-${version}.tar.gz";
+        sha256 = "sha256-OovvwJ6cwiktccqkPdTXy8IvS4EdYLrIxqnB8Dz2sWM=";
+      };
+
+      sourceRoot = ".";
+      nativeBuildInputs = [ autoPatchelfHook ];
+
+      installPhase = ''
+        install -m755 -d $out/bin/
+        install -m755 -D ziti/* $out/bin/
+      '';
+
+      meta = with lib; {
+        homepage = "https://github.com/openziti/ziti";
+        description = "The parent project for OpenZiti. Here you will find the executables for a fully zero trust, application embedded, programmable network";
         platforms = platforms.linux;
       };
     };
