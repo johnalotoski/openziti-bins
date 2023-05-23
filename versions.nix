d9d132c69e5ea9dc5779c64fbbb84894f3ec31d8
John Lotoski <john.lotoski@iohk.io>
2023-05-22 19:18:35 -0500

    bump: ziti-edge-tunnel v0.21.3

--- versions.nix
+++ versions.nix
@@ -64,7 +64,12 @@ in {
     in rec {
 
       x86_64-linux = rec {
-        latest = v0-20-22;
+        latest = v0-21-3;
+
+        v0-21-3 = {
+          version = "0.21.3";
+          hash = "sha256-O9vA08WVEond+R447ZLyYBgDSaj2VHp+5C20FxxYWUc=";
+        };
 
         v0-20-22 = {
           version = "0.20.22";
@@ -108,7 +113,12 @@ in {
       };
 
       x86_64-darwin = rec {
-        latest = v0-20-22;
+        latest = v0-21-3;
+
+        v0-21-3 = {
+          version = "0.21.3";
+          hash = "sha256-e2TUDH9A+nriVgHEHg9uY+3tvj5p0ArtRS9Kgsitwwg=";
+        };
 
         v0-20-22 = {
           version = "0.20.22";
@@ -122,7 +132,12 @@ in {
       };
 
       aarch64-darwin = rec {
-        latest = v0-20-22;
+        latest = v0-21-3;
+
+        v0-21-3 = {
+          version = "0.21.3";
+          hash = "sha256-TAFbQ0B+Kg2SvyW/JvG77aZfZRgaw3kA8XQRGr93AR4=";
+        };
 
         v0-20-22 = {
           version = "0.20.22";
